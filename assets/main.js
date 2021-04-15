701b7d8694d21b5681362cff2f9e77454a968db4
LCFirm <leo.cortez55@gmail.com>
2021-04-14 21:53:11 -0400

    Added First Upgrade

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,48 @@ function gemClick(number) {
 
 
 
-let baseCost = 25;
+
+let numOfEchestUp = 0;
+let numOfChestUp = 0;
+
+function buyEChestUpgrade() {
+    
+    let cost = Math.floor(25 * Math.pow(1.1, numOfEchestUp))
+    
+    if (gems >= cost) {
+        numOfEchestUp = numOfEchestUp + 1;
+        gems = gems - cost;
+        document.getElementById('gem-count').innerHTML = gems;
+        document.getElementById('echest-level').innerHTML = `EnderChest Level: ${numOfEchestUp}`;
+    };
+    
+    let nextCost = Math.floor(25 * Math.pow(1.1, numOfEchestUp));
+    document.getElementById('echest-cost').innerHTML = `Cost: ${nextCost}`;
+
+}
+
+
+
+function save() {
+    let save = {
+        gems: gems,
+        numOfEchestUp: numOfEchestUp,
+        numOfChestUp: numOfChestUp
+    }
+    
+}
+
+
+localStorage.setItem("save", JSON.stringify(save));
+
+function load() {
+    let savegame = JSON.parse(localStorage.getItem("save"));
+}
+window.setInterval(function() {
+    gemClick(numOfEchestUp);
+    save();
+    
+}, 1000);
 
 
 
