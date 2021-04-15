58f6309a5aac4515dc2f17b441d843d54f6107fc
LCFirm <leo.cortez55@gmail.com>
2021-04-14 21:11:35 -0400

    First Deployment

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+
+let gems = 0;
+
+function gemClick(number) {
+    gems = gems + number;
+    document.getElementById('gem-count').innerHTML = gems;
+    
+};
+
+
+
+let baseCost = 25;
+
+
+
+
+
+
