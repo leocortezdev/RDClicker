6f8b131d6fcfe30a1eb0be173e2521b7aa8d4724
LCFirm <leo.cortez55@gmail.com>
2021-04-15 12:46:01 -0400

    Fixed Dynamic Logic/ Fixed minor bugs

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,108 @@
 
-let gems = 0;
+let gameData = {
+    gems: 0,
+    gemsPerClick: 1,
+    numOfEchestUp: 0,
+    numOfChestUp: 0,
+    baseCost: 25,
+    valueCheck: false
+}
 
-function gemClick(number) {
-    gems = gems + number;
-    document.getElementById('gem-count').innerHTML = gems;
+
+
+function gemClick() {
+    gameData.gems += gameData.gemsPerClick;
+    document.getElementById('gem-count').innerHTML = gameData.gems;
     
 };
 
+function autoGemClick() {
+    gameData.gems = gameData.gems + gameData.numOfEchestUp;
+    document.getElementById('gem-count').innerHTML = gameData.gems;
+}
 
 
 
-let numOfEchestUp = 0;
-let numOfChestUp = 0;
 
 function buyEChestUpgrade() {
     
-    let cost = Math.floor(25 * Math.pow(1.1, numOfEchestUp))
+    let cost = Math.floor(gameData.baseCost * Math.pow(1.1, gameData.numOfEchestUp));
     
-    if (gems >= cost) {
-        numOfEchestUp = numOfEchestUp + 1;
-        gems = gems - cost;
-        document.getElementById('gem-count').innerHTML = gems;
-        document.getElementById('echest-level').innerHTML = `EnderChest Level: ${numOfEchestUp}`;
+    if (gameData.gems >= cost) {
+        gameData.numOfEchestUp = gameData.numOfEchestUp + 1;
+        gameData.gems = gameData.gems - cost;
+        document.getElementById('gem-count').innerHTML = gameData.gems;
+        document.getElementById('echest-level').innerHTML = `EnderChest Level: ${gameData.numOfEchestUp}`;
     };
     
-    let nextCost = Math.floor(25 * Math.pow(1.1, numOfEchestUp));
+    let nextCost = Math.floor(25 * Math.pow(1.1, gameData.numOfEchestUp));
     document.getElementById('echest-cost').innerHTML = `Cost: ${nextCost}`;
 
 }
 
 
-
-function save() {
-    let save = {
-        gems: gems,
-        numOfEchestUp: numOfEchestUp,
-        numOfChestUp: numOfChestUp
-    }
+function buyChestUpgrade() {
+    
+    let cost = Math.floor(gameData.baseCost * Math.pow(1.1, gameData.numOfChestUp));
+    if (gameData.gems >= cost) {
+        gameData.numOfChestUp = gameData.numOfChestUp + 1;
+        if (gameData.numOfChestUp > 0) {
+            gameData.gemsPerClick = gameData.numOfChestUp + 1;
+        } else {
+            gameData.gemsPerClick = gameData.numOfChestUp;
+        }
+        gameData.gems = gameData.gems - cost;
+        document.getElementById('chest-level').innerHTML = `Chest Level: ${gameData.numOfChestUp}`;
+        
+    };
+    let nextCost = Math.floor(gameData.baseCost * Math.pow(1.1, gameData.numOfChestUp));
+    document.getElementById('chest-cost').innerHTML = `Cost: ${nextCost}` 
     
 }
 
 
-localStorage.setItem("save", JSON.stringify(save));
+
+
+function valueCheck() {
+
+    if (gameData.gems < 25 && gameData.valueCheck === false) {
+        document.getElementById('upgradeArea').className = 'upgradeAreaOff';
+        
+        
+    } else {
+        document.getElementById('upgradeArea').className = 'upgradeAreaOn';
+        gameData.valueCheck = true;
+        document.getElementById('current-goal').innerHTML = 'CURRENT GOAL: REACH 10000 GEMS';
+    }
+
+}
+// Storage Functions
+function AutoSave() {
+    
+    localStorage.setItem("gameData", JSON.stringify(gameData));
+}
+
+function deleteSave() {
+    localStorage.removeItem("gameData");
+}
 
 function load() {
-    let savegame = JSON.parse(localStorage.getItem("save"));
+    let savegame = JSON.parse(localStorage.getItem("gameData"));
+    if (savegame !== null) {
+        gameData = savegame;
+    }
+    if (typeof savegame.gems !== "undefined") {gameData.gems = savegame.gems};
+    if (typeof savegame.numOfEchestUp !== "undefined") {gameData.numOfEchestUp = savegame.numOfEchestUp};
+    if (typeof savegame.numOfChestUp !== "undefined") {gameData.numOfChestUp = savegame.numOfChestUp};
 }
 window.setInterval(function() {
-    gemClick(numOfEchestUp);
-    save();
+    autoGemClick();
+    valueCheck();
+    AutoSave();
+    console.log(gameData.gemsPerClick);
+    console.log(gameData.numOfChestUp);
+    console.log(gameData.numOfEchestUp);
+    console.log(gameData.gems);
     
 }, 1000);
 
